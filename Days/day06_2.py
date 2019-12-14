9aa61b37dfcc248734d9e81282ea3c54355b6ea9
Jarik <jarikgerbertkarsten@gmail.com>
2019-12-14 13:45:27 +0100

    helper class

--- Days/day06_2.py
+++ Days/day06_2.py
@@ -1,3 +1,3 @@
 g,bfs,f={},lambda s, n, d, v: d if n == s else min([bfs(s,e,d+1,n) if e != v else 10**9 for e in g[n]]+[10**9]),"../inputs/input_day6.txt"
 for (a, b) in [l.strip().split(")") for l in open(f)]:g[a],g[b]=[b] if a not in g else g[a]+[b],[a] if b not in g else g[b]+[a]
-print(bfs("SAN","YOU",0,"")-2) # sorry couldnt help myself
+print(bfs("SAN","YOU",0,"")-2)
